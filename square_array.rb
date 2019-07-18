621b675fa81af8711ee41d100a026d049513d4fe
Patrick Harrison <jahman@bellatlantic.net>
2019-07-18 21:48:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,3 +4,12 @@
 # array=[1,2,3]
 #   array.each {|num|num **2} 
 # end
+def square_array(array)
+
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
+end
